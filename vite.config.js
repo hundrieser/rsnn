3647c3e96d6eabe35e195a40bd1c5b099c00d952
Shayan Hundrieser <hundrieser@MacBook-Pro-von-Shayan.local>
2025-10-08 17:31:51 +0200

    typo correction

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/rsnn',
+  base: '/rsnn/',
   plugins: [react()],
   resolve: {
     alias: {
